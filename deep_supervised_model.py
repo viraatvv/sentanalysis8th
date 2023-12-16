59d11df6f885e4a87a7108438ef483bf42bc8352
viraatv <145924354+viraatv@users.noreply.github.com>
2023-12-16 12:44:51 -0600

    Update deep_supervised_model.py

--- deep_supervised_model.py
+++ deep_supervised_model.py
@@ -18,6 +18,7 @@ y = reviews['sentiment']
 # Splitting the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
 
+# Transforming the data into something an AI can use
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(X_train)
 
@@ -34,10 +35,11 @@ X_train_padded = tokenize_and_pad(X_train, tokenizer, maxlen)
 X_test_padded = tokenize_and_pad(X_test, tokenizer, maxlen)
 
 # Building the model
-model = Sequential()
 voc = len(tokenizer.index_word) + 1
 feats = 8
 seq_len = 256
+
+model = Sequential()
 model.add(Embedding(voc, feats, input_length=seq_len))
 model.add(Flatten())
 model.add(Dropout(0.25))
@@ -52,11 +54,11 @@ start_time = time.time()  # Start recording time
 history = model.fit(X_train_padded, y_train, epochs=20, validation_data=(X_test_padded, y_test), callbacks=[callback])
 end_time = time.time()  # End recording time
 
-# Calculating the training duration
+# Time data
 training_duration = end_time - start_time
 print(f"Training duration: {training_duration} seconds")
 
-# Plotting accuracy and loss over time during training
+# Data visualization
 plt.figure(figsize=(12, 4))
 
 plt.subplot(1, 2, 1)
